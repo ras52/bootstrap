770e48e05834e2118805d2668de276f70219f84d
richard <richard>
2013-02-22 02:08:45 +0000

    Some fixes to malloc.

--- stage-4/malloc.c
+++ stage-4/malloc.c
@@ -10,6 +10,7 @@
  *   bool           is_free;
  *   struct header* next;
  *   struct header* prev;
+ *   void*          page_start;     // effectively a page id
  * }; */
 
 static
@@ -26,12 +27,13 @@ __find_blk( last, size ) {
 static
 __new_blk( size ) {
     /* Allocate a new block for at least SIZE bytes and prepend to __heap */
-    auto blksz = size > 0x0FF0 ? size : 0x0FF0;  /* 0x0FF0 == 0x1000 - 16 */
-    auto p = mmap(0, blksz + 16, 0x3, 0x22, -1, 0);
+    auto blksz = size > 0x0FEC ? size : 0x0FEC;  /* 0x0FEC == 0x1000 - 20 */
+    auto p = mmap(0, blksz + 20, 0x3, 0x22, -1, 0);
     p[0] = blksz;
     p[1] = 1;
     p[2] = __heap;
     p[3] = 0;
+    p[4] = p;
     if (__heap) __heap[3] = p;
     __heap = p;
     return p;
@@ -41,12 +43,13 @@ static
 __frag( blk, size ) {
     /* If the block BLK is significantly bigger than SIZE bytes, then fragment
      * it into a block of exactly SIZE bytes and a second block for the rest */
-    if ( blk[0] >= size + 32 ) {
-        auto b2 = blk + size + 16;
-        b2[0] = blk[0] - size - 16;
+    if ( blk[0] >= size + 20 + 4 ) {
+        auto b2 = blk + size + 20;
+        b2[0] = blk[0] - size - 20;
         b2[1] = 1;
         b2[2] = blk[2];
         b2[3] = blk;
+        b2[4] = blk[4];
         if (b2[2]) b2[2][3] = b2;
         blk[0] = size;
         blk[2] = b2;
@@ -57,16 +60,16 @@ static
 __defrag2( blk, b2 ) {
     /* The block at BLK and the next block, which is at B2, are both empty
      * so coalesce them into a single block. */
-    blk[0] += b2[0] + 16;
+    blk[0] += b2[0] + 20;
     blk[2] = b2[2];
     if (blk[2]) blk[2][3] = blk;
 }
 
 static
 __defrag( blk ) {
-    /* See whether the block at BLK can be coalesced with either neighour */
-    if ( blk[2] && blk[2][1] ) __defrag2( blk, blk[2] );
-    if ( blk[3] && blk[3][1] ) __defrag2( blk[3], blk );
+    /* See whether the block at BLK can be coalesced with either neighbour */
+    if ( blk[2] && blk[4] == blk[2][4] && blk[2][1] ) __defrag2( blk, blk[2] );
+    if ( blk[3] && blk[4] == blk[3][4] && blk[3][1] ) __defrag2( blk[3], blk );
 }
 
 /* The C library malloc() */
@@ -75,13 +78,13 @@ malloc( size ) {
     if (!p) p  = __new_blk( size );
     __frag( p, size );
     p[1] = 0;
-    return p + 16;
+    return p + 20;
 }
 
 /* The C library free() */
 free( ptr ) {
     if (ptr) {
-        auto p = ptr - 16;
+        auto p = ptr - 20;
         if (p[1]) _error(); /* Double free */
         p[1] = 1;
         __defrag( p );
@@ -90,8 +93,8 @@ free( ptr ) {
 
 /* The C library realloc() */
 realloc( ptr, size ) {
-    auto h = ptr - 16;
-    if ( h[2] && h[2][1] && h[0] + h[2][0] + 16 >= size ) {
+    auto h = ptr - 20;
+    if ( h[2] && h[4] == h[2][4] && h[2][1] && h[0] + h[2][0] + 20 >= size ) {
         __defrag2( h, h[2] );
         __frag( h, size );
         return ptr;
